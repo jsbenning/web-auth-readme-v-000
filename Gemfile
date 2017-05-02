6f4c42556155f53ff33cbd8f4f80f762f255c302
Joe <jsbenning4@gmail.com>
2017-05-02 14:15:21 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'faraday'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'dotenv-rails'
   gem 'byebug'
 end
 
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,15 @@ class SearchesController < ApplicationController
   def search
   end
 
+  def friends
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20170501'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+
   def foursquare
 
     client_id = ENV['FOURSQUARE_CLIENT_ID']
@@ -10,7 +19,7 @@ class SearchesController < ApplicationController
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
+      req.params['v'] = '20170501'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
